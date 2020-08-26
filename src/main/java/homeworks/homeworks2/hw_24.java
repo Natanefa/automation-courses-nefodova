1cc563a5b85cc17cee647ac60288b3c7af5be6b7
Nataliia Nefodova <nataliianefodova@gmail.com>
2020-08-26 22:00:56 +0300

    и еще две задачки, достала уже наверняка

---
+++ src/main/java/homeworks/homeworks2/hw_24.java
@@ -0,0 +1,11 @@
+package homeworks.homeworks2;
+
+public class hw_24 {
+    public static void main(String[] args) {
+        double correctnumber = 500;
+        double wrongnumber = 2555;
+        double res = correctnumber - wrongnumber / 4;
+
+        System.out.println(res);
+    }
+}
