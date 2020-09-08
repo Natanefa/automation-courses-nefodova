b997f79d709fbe7488c045db12676e96ce19247e
Nataliia Nefodova <nataliianefodova@gmail.com>
2020-09-08 23:24:47 +0300

    add dz7

--- src/main/java/homework_dz7/hw_task2.java
+++ src/main/java/homework_dz7/hw_task2.java
@@ -10,20 +10,45 @@ public class hw_task2 {
         System.out.println(LowerCaseStr);
 
         int words = 0;
+        int maxLength = 0;
         for (String word : str.split(" ")){
+            if (word.length() > maxLength){
+                maxLength = word.length();
+            }
             words = words + 1;
             String UpperWord = word.substring(0,1).toUpperCase() + word.substring(1);
             System.out.print(UpperWord + " ");
         }
         System.out.println(" ");
+        for (int i = 1; i < maxLength; i++) {
+            int wordCount = 0;
+            for (String word : str.split(" ")){
+                if (word.length() == i) {
+                    wordCount = wordCount +1;
+                }
+            }
+            if(wordCount > 0) {
+                System.out.println(i + " " + wordCount);
+            }
+        }
+
+        System.out.println(" ");
+        System.out.println("max = " + maxLength);
         System.out.println("Words count = " + words);
         System.out.println("");
         System.out.println(str.replaceAll("a", "@"));
+        System.out.println(str.replaceAll("c", " "));
 
         int letters = 0;
         for (Character letter : str.toCharArray()) {
             letters = letters +1;
         }
         System.out.println("Character count = " + letters);
+
+        char [] Text = {'h', 'e', 'l', 'p'};
+        System.out.println(String.copyValueOf(Text, 0, 4));
+        System.out.println(String.copyValueOf(Text, 1, 3) + String.copyValueOf(Text, 0, 1));
+        System.out.println(String.copyValueOf(Text, 2, 2) + String.copyValueOf(Text, 0, 2));
+        System.out.println(String.copyValueOf(Text, 3, 1) + String.copyValueOf(Text, 0, 3));
     }
 }
