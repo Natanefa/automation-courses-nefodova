0046ec55b5abcd091168ce071a8ac658102cd5db
Nataliia Nefodova <nataliianefodova@gmail.com>
2020-09-16 23:30:06 +0300

    add task timelogger

--- src/main/java/homework_dz9/res_constr.java
+++ src/main/java/homework_dz9/res_constr.java
@@ -6,9 +6,16 @@ public class res_constr {
     public static void main(String[] args) {
         hw_task1 stringGen = new hw_task1();
         System.out.println(stringGen.stringGenerator("numeric", 10));
-        System.out.println(stringGen.stringGenerator("alphanumeric" , 10));
+        System.out.println(stringGen.stringGenerator("alphanumeric", 10));
         System.out.println(stringGen.stringGenerator("alpha", 10));
+        System.out.println(" ");
 
+
+        TimeLogger logger = new TimeLogger();
+        for (int i = 0; i < 10; i++) {
+            System.out.println(logger.log("operation" + i));
         }
+
+    }
 
 }
